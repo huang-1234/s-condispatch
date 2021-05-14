66696c5b15fcecfd36dd4290e1f46e02e6882a20
hsq <137707531@qq.com>
2021-05-15 00:04:29 +0800

    build database and ach getArticleList

---
+++ app/controller/default/home.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx, app } = this;
+    const result = await app.mysql.get('article', {});
+    console.log(result);
+    ctx.body = result;
+  }
+  async getArticleList() {
+    const { ctx, app } = this;
+    const sql = 'SELECT article.id as id,' +
+      'article.title as title, ' +
+      'article.introduce as introduce,' +
+      'article.article_content as article_content,' +
+      "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime," +
+      'article.view_count as view_count,' +
+      'type.typeName as typeName ' +
+      'FROM article LEFT JOIN type ON article.type_id = type.Id';
+    const results = await app.mysql.query(sql);
+    console.log('getArticleList:', results);
+    ctx.body = {
+      data: results,
+    };
+  }
+
+  //
+  async getArticleById() {
+    // 先配置路由的动态传值，然后再接收值
+    // let id = this.ctx.params.id;
+    const { ctx } = this;
+
+    const sql = 'SELECT article.id as id,' +
+      'article.title as title,' +
+      'article.introduce as introduce,' +
+      'article.article_content as article_content,' +
+      "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime," +
+      'article.view_count as view_count ,' +
+      'type.typeName as typeName ,' +
+      'type.id as typeId ' +
+      'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
+      'WHERE article.id=' + ctx.params.id;
+    //
+    const result = await this.app.mysql.query(sql);
+    this.ctx.body = { data: result };
+
+  }
+
+  // 得到类别名称和编号
+  async getTypeInfo() {
+    const { app, ctx } = this;
+    const result = await app.mysql.select('type');
+    ctx.body = {
+      data: result,
+    };
+  }
+}
+
+module.exports = HomeController;
---
+++ config/config.default.js
@@ -0,0 +1,60 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1620896559869_5567';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // 配置mysql
+  config.mysql = { // database configuration
+    client: {
+      // host
+      host: 'localhost',
+      // port
+      port: '3306',
+      // username
+      user: 'root',
+      // password
+      password: 'huang',
+      // database
+      database: 'react_blog',
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
+  // 配置跨域相关的
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+    domainWhiteList: [ '*' ],
+  };
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+  };
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
